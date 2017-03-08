9acb2457395aba218ec346022d8ac9ebea2c2ac9
Behrooz Karjoo <bkarjoo@gmail.com>
2017-03-08 12:41:59 -0500

    Checks if there are missing backtests for any live runs

---
+++ CheckIfThereAreMissingBackTests.sql
@@ -0,0 +1,11 @@
+select 
+    t.BBox
+  , convert(date, t.RunDateStart) as dd
+from [PrimusGroup].[dbo].[PrmsBlotter] t
+where t.RunType = 'Live'
+  and not exists (
+    select 1
+    from [PrimusGroup].[dbo].[PrmsBlotter] i
+    where i.RunType = 'Backtesting'
+      and i.BBox = t.BBox
+      and convert(date,i.TestDateFrom) = convert(date,t.RunDateStart)
