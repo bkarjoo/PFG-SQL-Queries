8aaebadd10c69919d28071f82104650106e2176d
Behrooz Karjoo <bkarjoo@gmail.com>
2017-03-08 11:47:55 -0500

    add where clause is backtest

--- IdentifyDuplicateBackTests.sql
+++ IdentifyDuplicateBackTests.sql
@@ -1,3 +1,4 @@
 SELECT COUNT(*), TestDateFrom, BBox, RunType, CONVERT(date, RunDateStart) As 'RunDate' FROM [PrimusGroup].[dbo].[PrmsBlotter]
+WHERE RunType = 'Backtesting' 
 GROUP BY  TestDateFrom, BBox, RunType, CONVERT(date, RunDateStart)
 HAVING COUNT(*) > 1
